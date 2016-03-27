096028beca2aebc16486ecac6aec671dd31d0878
Yeong Sheng, Tan <yeongsheng.tan@ruckuswireless.com>
2016-03-28 00:31:01 +0800

    feat: Type Annotations

--- seat_saver/web/elm/SeatSaver.elm
+++ seat_saver/web/elm/SeatSaver.elm
@@ -1,8 +1,9 @@
 module SeatSaver where
 
-import Html exposing (ul, li, text)
+import Html exposing (..)
 import Html.Attributes exposing (class)
 
+main : Html
 main =
   view init
 
@@ -18,6 +19,7 @@ type alias Seat =
 type alias Model =
   List Seat
 
+init : Model
 init =
   [ { seatNo = 1, occupied = False }
   , { seatNo = 2, occupied = False }
@@ -35,8 +37,11 @@ init =
 
 -- VIEW
 
+
+view : Model -> Html
 view model =
   ul [ class "seats" ] (List.map seatItem model)
 
+seatItem : Seat -> Html
 seatItem seat =
   li [ class "seat available" ] [ text (toString seat.seatNo) ]
