508e5aebd81ffcd85a55369433969688dce4bcb2
Yeong Sheng, Tan <yeongsheng.tan@ruckuswireless.com>
2016-04-03 09:22:58 +0800

    feat: phoenix data API and Effects impl for json parsing

--- seat_saver/web/elm/SeatSaver.elm
+++ seat_saver/web/elm/SeatSaver.elm
@@ -6,6 +6,8 @@ import Html.Events exposing (onClick)
 import StartApp
 import Effects exposing (Effects, Never)
 import Task exposing (Task)
+import Http
+import Json.Decode as Json exposing ((:=))
 
 app =
   StartApp.start
@@ -36,23 +38,7 @@ type alias Model =
 
 init : (Model, Effects Action)
 init =
-  let
-    seats =
-      [ { seatNo = 1, occupied = False }
-      , { seatNo = 2, occupied = False }
-      , { seatNo = 3, occupied = False }
-      , { seatNo = 4, occupied = False }
-      , { seatNo = 5, occupied = False }
-      , { seatNo = 6, occupied = False }
-      , { seatNo = 7, occupied = False }
-      , { seatNo = 8, occupied = False }
-      , { seatNo = 9, occupied = False }
-      , { seatNo = 10, occupied = False }
-      , { seatNo = 11, occupied = False }
-      , { seatNo = 12, occupied = False }
-      ]
-  in
-    (seats, Effects.none)
+  ([], fetchSeats)
 
 -- VIEW
 
@@ -76,7 +62,7 @@ seatItem address seat =
 -- UPDATE
 
 
-type Action = Toggle Seat
+type Action = Toggle Seat | SetSeats (Maybe Model)
 
 update : Action -> Model -> (Model, Effects Action)
 update action model =
@@ -89,3 +75,28 @@ update action model =
           else seatFromModel
       in
         (List.map updateSeat model, Effects.none)
+    SetSeats seats ->
+      let
+        newModel = Maybe.withDefault model seats
+      in
+        (newModel, Effects.none)
+
+-- EFFECTS
+
+
+fetchSeats: Effects Action
+fetchSeats =
+  Http.get decodeSeats "http://localhost:4000/api/seats"
+    |> Task.toMaybe
+    |> Task.map SetSeats
+    |> Effects.task
+
+decodeSeats: Json.Decoder Model
+decodeSeats =
+  let
+    seat =
+      Json.object2 (\seatNo occupied -> (Seat seatNo occupied))
+        ("seatNo" := Json.int)
+        ("occupied" := Json.bool)
+  in
+    Json.at ["data"] (Json.list seat)
