68bef6ddfe1d01741d8006f0a2ae98ecb4aaafc3
Yeong Sheng, Tan <yeongsheng.tan@ruckuswireless.com>
2016-04-07 18:06:49 +0800

    feat: init data for Elm App over Phoenix Channels

--- seat_saver/web/elm/SeatSaver.elm
+++ seat_saver/web/elm/SeatSaver.elm
@@ -12,7 +12,7 @@ app =
     { init = init
     , update = update
     , view = view
-    , inputs = []
+    , inputs = [incomingActions]
     }
 
 main : Signal Html
@@ -36,23 +36,7 @@ type alias Model =
 
 init : (Model, Effects Action)
 init =
-  let
-    seats =
-      [ { seatNo = 1, occupied = False }
-      , { seatNo = 2, occupied = False }
-      , { seatNo = 3, occupied = False }
-      , { seatNo = 4, occupied = False }
-      , { seatNo = 5, occupied = False }
-      , { seatNo = 6, occupied = False }
-      , { seatNo = 7, occupied = False }
-      , { seatNo = 8, occupied = False }
-      , { seatNo = 9, occupied = False }
-      , { seatNo = 10, occupied = False }
-      , { seatNo = 11, occupied = False }
-      , { seatNo = 12, occupied = False }
-      ]
-  in
-    (seats, Effects.none)
+  ([], Effects.none)
 
 -- VIEW
 
@@ -76,7 +60,7 @@ seatItem address seat =
 -- UPDATE
 
 
-type Action = Toggle Seat
+type Action = Toggle Seat | SetSeats Model
 
 update : Action -> Model -> (Model, Effects Action)
 update action model =
@@ -89,3 +73,14 @@ update action model =
           else seatFromModel
       in
         (List.map updateSeat model, Effects.none)
+    SetSeats seats ->
+      (seats, Effects.none)
+
+
+ -- SIGNALS
+
+
+port seatLists : Signal Model
+incomingActions: Signal Action
+incomingActions =
+  Signal.map SetSeats seatLists
--- seat_saver/web/static/js/socket.js
+++ seat_saver/web/static/js/socket.js
@@ -54,9 +54,13 @@ let socket = new Socket("/socket", {params: {token: window.userToken}})
 socket.connect()
 
 // Now that you are connected, you can join channels with a topic:
-let channel = socket.channel("topic:subtopic", {})
-channel.join()
-  .receive("ok", resp => { console.log("Joined successfully", resp) })
-  .receive("error", resp => { console.log("Unable to join", resp) })
+// let channel = socket.channel("seats:planner", {})
+// channel.join()
+//   .receive("ok", resp => { console.log("Joined successfully", resp) })
+//   .receive("error", resp => { console.log("Unable to join", resp) })
+
+// channel.on('set_seats', data => {
+//   console.log('got seats', data.seats)
+// })
 
 export default socket
